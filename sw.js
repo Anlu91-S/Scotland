f55496f24fc5527ca418661646df4e41a0b19fe8
Anlu91-S <a.lundell@hotmail.com>
2026-07-20 18:20:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const CACHE = 'scotland-roadbook-v1';
-const CORE = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CORE = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png',
+'./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
 });
